478fde8e14e13ffbd642f580200690bc647479de
Mazen Talaat <mazen.haggag29@gmail.com>
2024-03-19 12:31:27 +0200

    Robot subscribing

--- Assets/Scripts/RobotController.cs
+++ Assets/Scripts/RobotController.cs
@@ -5,43 +5,31 @@ using UnityEngine;
 
 public class RobotController : MonoBehaviour
 {
+    // Get all the joints gameobejects
     public List<GameObject> jointsList = new List<GameObject>();
+    // Get a ref. to the JointsSubscriber to access the data
     public JointsSubscriber jointsSubscriber;
-    // Start is called before the first frame update
-    void Start()
-    {
-        // TODO Know each link angle to rotate
-        jointsList[0].transform.Rotate(10, 0, 0);
-        jointsList[1].transform.eulerAngles = new Vector3(0, 0, 30);
-        jointsList[2].transform.eulerAngles = new Vector3(0, 20, 0);
-        jointsList[3].transform.eulerAngles = new Vector3(0, 0, 0);
-        jointsList[4].transform.eulerAngles = new Vector3(40, 0, 0);
-        jointsList[5].transform.eulerAngles = new Vector3(0, 50, 0);
+    // Angles received
+    float[] jointsAngles = new float[6];
 
-        print("working");
-    }
-
-    // TODO Delay the start of the update function
     void Update()
     {
-        //string[] splitNumbers = jointsSubscriber.messageData.Split(',');
-
-        //// Convert the string array to a float array
-        //float[] floatNumbers = new float[splitNumbers.Length];
-        //for (int i = 0; i < splitNumbers.Length; i++)
-        //{
-        //    floatNumbers[i] = float.Parse(splitNumbers[i]);
-        //    //print(floatNumbers[i]);
-        //}
-
-        //// Now, floatNumbers contains the float numbers
-        //jointsList[0].transform.Rotate(10, 0, 0);
-        //jointsList[1].transform.eulerAngles = new Vector3(0, 0, 30);
-        //jointsList[2].transform.eulerAngles = new Vector3(0, 20, 0);
-        //jointsList[3].transform.eulerAngles = new Vector3(0, 0, 0);
-        //jointsList[4].transform.eulerAngles = new Vector3(40, 0, 0);
-        //jointsList[5].transform.eulerAngles = new Vector3(0, 50, 0);
-
-        //print("working");
+        // Check if message > 10 to prevent errors in splitting
+        if (jointsSubscriber.messageData.Length > 10)
+        {
+            string[] splitNumbers = jointsSubscriber.messageData.Split(',');
+            //Parse each angle rad->deg
+            for (int i = 0; i < splitNumbers.Length; i++)
+            {
+                jointsAngles[i] = float.Parse(splitNumbers[i]) * 180 / Mathf.PI;
+            }
+            // Rotate each gameobject
+            jointsList[0].transform.localEulerAngles = new Vector3(jointsList[0].transform.localEulerAngles.x, -jointsAngles[0], jointsList[0].transform.localEulerAngles.z);
+            jointsList[1].transform.localEulerAngles = new Vector3(jointsAngles[1], jointsList[1].transform.localEulerAngles.y, jointsList[1].transform.localEulerAngles.z);
+            jointsList[2].transform.localEulerAngles = new Vector3(jointsAngles[2], jointsList[2].transform.localEulerAngles.y, jointsList[2].transform.localEulerAngles.z);
+            jointsList[3].transform.localEulerAngles = new Vector3(jointsList[3].transform.localEulerAngles.x, jointsList[3].transform.localEulerAngles.y, -jointsAngles[3]);
+            jointsList[4].transform.localEulerAngles = new Vector3(jointsAngles[4], jointsList[4].transform.localEulerAngles.y, jointsList[4].transform.localEulerAngles.z);
+            jointsList[5].transform.localEulerAngles = new Vector3(jointsList[5].transform.localEulerAngles.x, jointsList[5].transform.localEulerAngles.y, -jointsAngles[5]);
+        }
     }
 }
