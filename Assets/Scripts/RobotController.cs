1b9ec3e9a50e2bc8fbb761ddd2736e62a4c320b9
Mazen Talaat <mazen.haggag29@gmail.com>
2024-03-19 13:22:13 +0200

    Bug fix

--- Assets/Scripts/RobotController.cs
+++ Assets/Scripts/RobotController.cs
@@ -14,22 +14,30 @@ public class RobotController : MonoBehaviour
 
     void Update()
     {
-        // Check if message > 10 to prevent errors in splitting
-        if (jointsSubscriber.messageData.Length > 10)
+        try
         {
-            string[] splitNumbers = jointsSubscriber.messageData.Split(',');
-            //Parse each angle rad->deg
-            for (int i = 0; i < splitNumbers.Length; i++)
+            // Check if message > 10 to prevent errors in splitting
+            if (jointsSubscriber.messageData.Length > 10)
             {
-                jointsAngles[i] = float.Parse(splitNumbers[i]) * 180 / Mathf.PI;
+                string[] splitNumbers = jointsSubscriber.messageData.Split(',');
+                //Parse each angle rad->deg
+                for (int i = 0; i < splitNumbers.Length; i++)
+                {
+                    jointsAngles[i] = float.Parse(splitNumbers[i]) * 180 / Mathf.PI;
+                }
+                // Rotate each gameobject
+                jointsList[0].transform.localEulerAngles = new Vector3(0, -jointsAngles[0], 0);
+                jointsList[1].transform.localEulerAngles = new Vector3(jointsAngles[1], 0, 0);
+                jointsList[2].transform.localEulerAngles = new Vector3(jointsAngles[2], 0, 0);
+                jointsList[3].transform.localEulerAngles = new Vector3(0, 0, -jointsAngles[3]);
+                jointsList[4].transform.localEulerAngles = new Vector3(jointsAngles[4], 0, 0);
+                jointsList[5].transform.localEulerAngles = new Vector3(0, 0, -jointsAngles[5]);
             }
-            // Rotate each gameobject
-            jointsList[0].transform.localEulerAngles = new Vector3(jointsList[0].transform.localEulerAngles.x, -jointsAngles[0], jointsList[0].transform.localEulerAngles.z);
-            jointsList[1].transform.localEulerAngles = new Vector3(jointsAngles[1], jointsList[1].transform.localEulerAngles.y, jointsList[1].transform.localEulerAngles.z);
-            jointsList[2].transform.localEulerAngles = new Vector3(jointsAngles[2], jointsList[2].transform.localEulerAngles.y, jointsList[2].transform.localEulerAngles.z);
-            jointsList[3].transform.localEulerAngles = new Vector3(jointsList[3].transform.localEulerAngles.x, jointsList[3].transform.localEulerAngles.y, -jointsAngles[3]);
-            jointsList[4].transform.localEulerAngles = new Vector3(jointsAngles[4], jointsList[4].transform.localEulerAngles.y, jointsList[4].transform.localEulerAngles.z);
-            jointsList[5].transform.localEulerAngles = new Vector3(jointsList[5].transform.localEulerAngles.x, jointsList[5].transform.localEulerAngles.y, -jointsAngles[5]);
         }
+        catch (System.Exception)
+        {
+            throw;
+        }
+        
     }
 }
